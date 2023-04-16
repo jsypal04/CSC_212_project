01ecc440898fc7fb95d70e756ac6aa062599f5bd
Joseph Sypal <60202320+jsypal04@users.noreply.github.com>
2023-04-16 13:40:50 -0400

    increased vocabulary

--- Demo.java
+++ Demo.java
@@ -7,18 +7,20 @@ public class Demo {
 
     // setup grammar production rules
     public static void setupGrammar() {
+        String[] s = {"<noun><verb>"};
+        grammar.put("<start>", s);
+
         // nouns
         String[] n1 = {"<person>", "<place>", "<thing>", "<idea>"};
         String[] n2 = {"<name>", "<article><occupation>", "<article><adjective><occupation>"}; // person
-        String[] n3 = {"<general>", "<adjective><general>", "<article><specific>", "<article><adjective><specific>"}; // idea
+        String[] n3 = {"<general>", "<adjective><general>"}; // idea
         String[] n4 = {"<article><thing noun>", "<article><adjective><thing noun>"}; // thing
         String[] n5 = {"<article><place noun>", "<article><adjective><place noun>"}; // place
-        String[] n6 = {"Joe ", "Hieu Bui "}; // name
-        String[] n7 = {"religion ", "love ", "math ", "CSC 212"}; // general
-        String[] n8 = {"Roman Catholic Church "}; // specific
-        String[] n9 = {"doctor ", "lawyer "}; // occupation
-        String[] n10 = {"desk ", "dog ", "chair "}; // thing noun
-        String[] n11 = {"Pryz ", "hospital ", "church "}; // place noun
+        String[] n6 = {"Joe ", "Hieu Bui ", "Luke Bubel ", "John Cardillo ", "Jeffery Koeferl "}; // name
+        String[] n7 = {"religion ", "love ", "math ", "CSC 212 "}; // general
+        String[] n9 = {"doctor ", "lawyer ", "teacher ", "developer ", "real estate agent ", "police officer ", "fireman ", "student ", "businessman ", "CEO ", "marketing assistant ", "freelancer ", "photographer ", "web developer ", "cashier "}; // occupation
+        String[] n10 = {"desk ", "dog ", "chair ", "street ", "building ", "window ", "cat ", "turtle ", "wall ", "stone ", "door ", "floor ", "painting ", "frame ", "lamp ", "cushion ", "alarm ", "fire ", "ladder "}; // thing noun
+        String[] n11 = {"Pryz ", "hospital ", "church ", "school ", "university ", "Garv ", "office ", "room ", "parking lot "}; // place noun
         String[] n12 = {"<noun>"}; // pred nom
         String[] n13 = {"<noun>"}; // direct object
 
@@ -29,7 +31,6 @@ public class Demo {
         grammar.put("<place>", n5);
         grammar.put("<name>", n6);
         grammar.put("<general>", n7);
-        grammar.put("<specific>", n8);
         grammar.put("<occupation>", n9);
         grammar.put("<thing noun>", n10);
         grammar.put("<place noun>", n11);
@@ -41,17 +42,19 @@ public class Demo {
         String[] v2 = {"<trans>", "<intrans>"};
         String[] v3 = {"<trans verb><direct object>", "<adverb><trans verb><direct object>"};
         String[] v4 = {"<intrans verb>", "<intrans verb><adverb>"};
-        String[] v5 = {"<link verb><pred nom>", "<link verb><pred adj>"};
-        String[] v6 = {"is ", "was ", "has ", "had ", "became "}; // linking verbs
-        String[] v7 = {"accepts ", "bothers ", "defines "}; // transitive verbs
-        String[] v8 = {"laughs ", "was laughing ", "smiles ", "was smiling ", "claps ", "was clapping "}; // intransitive verbs
+        String[] v5 = {"<link being><pred nom>", "<link being><pred adj>", "<link other><pred nom>"};
+        String[] v6 = {"is ", "was ", "became "}; // linking verbs
+        String[] v9 = {"has ", "had "};
+        String[] v7 = {"accepts ", "bothers ", "defines ", "destroyed ", "read ", "earns ", "earned ", "discovers ", "discovered ", "embarrasses ", "embarrassed ", "cleans ", "cleaned ", "gathers ", "gathered ", "messages ", "messaged ", "interrupts ", "interrupted "}; // transitive verbs
+        String[] v8 = {"laughs ", "was laughing ", "smiles ", "was smiling ", "claps ", "was clapping ", "ate ", "collapses ", "collapsed ", "was collapsing ", "falls ", "fell ", "was falling ", "learns ", "learned ", "was learning ", "reclines ", "reclined ", "sleeps ", "slept ", "walks ", "walked "}; // intransitive verbs
 
         grammar.put("<verb>", v1);
         grammar.put("<act>", v2);
         grammar.put("<trans>", v3);
         grammar.put("<intrans>", v4);
         grammar.put("<link>", v5);
-        grammar.put("<link verb>", v6);
+        grammar.put("<link being>", v6);
+        grammar.put("<link other>", v9);
         grammar.put("<trans verb>", v7);
         grammar.put("<intrans verb>", v8);
 
@@ -61,14 +64,14 @@ public class Demo {
         grammar.put("<article>", art);
 
         // adjectives
-        String[] adj1 = {"brown ", "bright "};
+        String[] adj1 = {"brown ", "bright ", "angry ", "clear ", "curious ", "dangerous ", "dull ", "fantastic ", "gifted ", "hungry ", "important ", "nasty ", "proud ", "selfish ", "shiny ", "stupid ", "talented ", "witty "};
         String[] adj2 = {"<adjective>"}; // pred adj
 
         grammar.put("<adjective>", adj1);
         grammar.put("<pred adj>", adj2);
 
         //adverbs
-        String[] adv1 = {"briliantly ", "unfortunatly ", "well "};
+        String[] adv1 = {"briliantly ", "unfortunatly ", "well ", "diligently ", "doubtfully ", "far ", "fast ", "longingly ", "loosely ", "loudly ", "happily ", "rapidly ", "rarely ", "sternly ", "stricly ", "warmly ", "too ", "tremendously ", "greedily "};
 
         grammar.put("<adverb>", adv1);
     }
@@ -101,6 +104,10 @@ public class Demo {
     public static void main(String[] args) {
         // run setup functions
         setupGrammar();
-        System.out.println(produce("<noun><verb>"));
+        
+
+        System.out.println();
+        System.out.println(produce("<start>"));
+        System.out.println();
     }
 }
